114d940d497b83cb23642c981825399579d850c9
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2025-08-31 19:36:09 -0300

    add api route

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post("/downloads", function (Request $request) {
+   \Illuminate\Support\Facades\Log::info("Download solicitado", [
+       "url" => $request->input("url"),
+       "ip" => $request->ip(),
+   ]);
+
+   return response()->json(["message" => "Url salva com sucesso!"]);
+});
