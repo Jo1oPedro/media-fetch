ea5e141bd8de3bc31fcfbb034aba0118634e41c6
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2025-09-01 00:16:13 -0300

    Add middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("download/index");
 });
 
-Route::get("/login", function () {
-   return view("auth/login");
+Route::middleware("guest")->group(function () {
+    Route::get("/login", [AuthController::class, "showLogin"])->name("login.form");
+    Route::post("/login", [AuthController::class, "login"])->name("login");
+
+    Route::get("/register", [AuthController::class, "showRegister"])->name("register.form");
+    Route::post("/register", [AuthController::class, "register"])->name("register");
 });
 
-Route::get("/download", function() {
-   return view("download/index");
+Route::middleware("auth")->group(function () {
+    Route::get("/download", function() {
+       return view("download/index");
+    })->name("download");
 });
