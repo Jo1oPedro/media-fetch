485e734db715445d9774960cf58aabb0d078e77f
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2025-09-03 11:05:18 -0300

    fix media download controller path and add user relations

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\MediaDownloadController;
+use App\Http\Controllers\Api\MediaDownloadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
